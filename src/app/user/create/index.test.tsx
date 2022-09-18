5d42ae3f9e7b7b3b1486bcd09012de9295a6113c
Germán Casares <german.casares@outlook.com>
2022-09-18 10:07:15 +0000

    moar tests

--- src/app/user/create/index.test.tsx
+++ src/app/user/create/index.test.tsx
@@ -1,8 +1,20 @@
 import { createRoot } from 'react-dom/client';
+import { act } from 'react-dom/test-utils';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Create from '.';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  createRoot(div).render(<Create />);
+  const root = createRoot(div);
+  const queryClient = new QueryClient();
+
+  act(() => root.render(
+    <Router>
+      <QueryClientProvider client={queryClient}>
+        <Create />
+      </QueryClientProvider>
+    </Router>,
+  ));
 });
