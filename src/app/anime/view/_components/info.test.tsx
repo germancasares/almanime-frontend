d2cfa179c1c0bed1d9173ae819eb512c688d1424
Germán Casares <german.casares@outlook.com>
2023-01-29 22:29:00 -0800

    As a user, I'd like to see more links in the anime view (#60)

--- src/app/anime/view/_components/info.test.tsx
+++ src/app/anime/view/_components/info.test.tsx
@@ -1,25 +1,80 @@
 import { createRoot } from 'react-dom/client';
+import { act } from 'react-dom/test-utils';
 
 import AnimeStatus from 'enums/AnimeStatus';
 import Season from 'enums/Season';
 
 import Info from './info';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  createRoot(div).render(
-    <Info
-      anime={{
-        id: 'abcd',
-        kitsuID: 1,
-        slug: 'aaaa',
-        name: 'AAAAAAAA',
-        season: Season.Summer,
-        status: AnimeStatus.Finished,
-        synopsis: '',
-        startDate: null,
-      }}
-      episodesCount={0}
-    />,
-  );
+describe('[Info]', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    createRoot(div).render(
+      <Info
+        anime={{
+          id: 'abcd',
+          kitsuID: 1,
+          slug: 'aaaa',
+          name: 'AAAAAAAA',
+          season: Season.Summer,
+          status: AnimeStatus.Finished,
+          synopsis: '',
+          startDate: null,
+        }}
+        episodesCount={0}
+      />,
+    );
+  });
+
+  it('renders episodes', () => {
+    const div = document.createElement('div');
+
+    act(() => {
+      createRoot(div).render(
+        <Info
+          anime={{
+            id: 'abcd',
+            kitsuID: 1,
+            slug: 'aaaa',
+            name: 'AAAAAAAA',
+            season: Season.Summer,
+            status: AnimeStatus.Finished,
+            synopsis: '',
+            startDate: null,
+          }}
+          episodesCount={1}
+        />,
+      );
+    });
+
+    expect(div.querySelector('li')?.querySelector('span')).toHaveTextContent('Episodes:');
+  });
+
+  it.each(([
+    [{ myAnimeListID: 1 }, 'https://myanimelist.net/anime/1'],
+    [{ aniListID: 1 }, 'https://anilist.co/anime/1'],
+    [{ aniDBID: 1 }, 'https://anidb.net/anime/1'],
+  ]))('Anime with %p renders link for %p', (anime, result: string) => {
+    const div = document.createElement('div');
+    act(() => {
+      createRoot(div).render(
+        <Info
+          anime={{
+            id: 'abcd',
+            kitsuID: 1,
+            slug: 'aaaa',
+            name: 'AAAAAAAA',
+            season: Season.Summer,
+            status: AnimeStatus.Finished,
+            synopsis: '',
+            startDate: null,
+            ...anime,
+          }}
+          episodesCount={0}
+        />,
+      );
+    });
+
+    expect(div.querySelector(`a[href='${result}']`)).toBeTruthy();
+  });
 });
