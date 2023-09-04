242a93e3f4b1b44706489dd4b835fa4bd245dc82
German Casares <german.casares@outlook.com>
2023-09-04 01:32:14 +0000

    Continue refresh

--- src/app/subtitle/editor/_components/player.stories.tsx
+++ src/app/subtitle/editor/_components/player.stories.tsx
@@ -1,53 +1,51 @@
-/* eslint-disable react/jsx-props-no-spreading */
-// eslint-disable-next-line storybook/story-exports
-import { Story } from '@storybook/react/types-6-0';
+import type { Meta, StoryObj } from '@storybook/react';
+import Player from './player';
 
-import Player, { PlayerProps } from './player';
-
-export default {
+const meta: Meta<typeof Player> = {
   title: 'Alm VideoJS Editor/Player',
   component: Player,
 };
 
-const Template: Story<PlayerProps> = (args) => (
-  <div>
-    <Player {...args} />
-  </div>
-);
+export default meta;
 
-export const Default = Template.bind({});
-Default.args = {
-  playerOptions: {
-    // autoplay: true,
-    // muted: true,
-    controls: true,
-    fluid: true,
-    responsive: true,
-    sources: [{
-      src: '/sample.mp4',
-      type: 'video/mp4',
-    }],
+type Story = StoryObj<typeof Player>;
+
+
+export const Default: Story = {
+  args: {
+    playerOptions: {
+      // autoplay: true,
+      // muted: true,
+      controls: true,
+      fluid: true,
+      responsive: true,
+      sources: [{
+        src: '/sample.mp4',
+        type: 'video/mp4',
+      }],
+    },
   },
 };
 
-export const DefaultWithSubtitles = Template.bind({});
-DefaultWithSubtitles.args = {
-  playerOptions: {
-    controls: true,
-    responsive: true,
-    sources: [{
-      src: '/OuterScienceSubs.mp4',
-      type: 'video/mp4',
-    }],
-  },
-  subtitleOptions: {
-    // subUrl: '/OuterScienceSubs.ass',
-    subContent: '',
-    fonts: [
-      'http://fonts.cdnfonts.com/css/gisha',
-      'http://fonts.cdnfonts.com/css/aharoni',
-    ],
-    onReady: () => {},
-    debug: true,
+export const DefaultWithSubtitles: Story = {
+  args: {
+    playerOptions: {
+      controls: true,
+      responsive: true,
+      sources: [{
+        src: '/OuterScienceSubs.mp4',
+        type: 'video/mp4',
+      }],
+    },
+    subtitleOptions: {
+      // subUrl: '/OuterScienceSubs.ass',
+      subContent: '',
+      fonts: [
+        'http://fonts.cdnfonts.com/css/gisha',
+        'http://fonts.cdnfonts.com/css/aharoni',
+      ],
+      onReady: () => {},
+      debug: true,
+    },
   },
 };
